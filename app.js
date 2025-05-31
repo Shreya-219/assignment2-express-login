79c081726c452bffcbad8efd0f4c1468894bdddb
Shreyaa-09 <b23cs058@kitsw.ac.in>
2025-05-31 19:22:22 +0530

    final commit

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
-const path = require("path");
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// View engine
+app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, "public")));
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("home"); 
+// Routes
+app.get('/', (req, res) => res.render('home'));
+
+app.get('/login', (req, res) => res.render('login'));
+
+app.get('/help', (req, res) => res.render('help'));
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === 'shreya' && password === '2109') {
+    res.send(`Login successful. Welcome, ${username}!`);
+  } else {
+    res.send('Invalid username or password');
+  }
 });
 
-app.get("/help", (req, res) => {
-  res.render("help"); 
-});
 
-app.get("/login", (req, res) => {
-  res.render("login"); 
-});
-
-// Start the server
+// Start server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
